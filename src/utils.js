014085911d561d41a3536f1c4cdebf772fef399f
lhmiao <2696254238@qq.com>
2019-12-03 16:12:58 +0800

    feat: add utils

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+export const getCsrfToken = (key = 'csrfToken') => {
+  const cookies = document.cookie
+    .split('; ')
+    .map(cookieString => cookieString.split('='));
+  for(const [cookieKey, cookieValue] of cookies) {
+    if (key === cookieKey) return cookieValue;
+  }
+  return undefined;
+};
+
+export const isCsrfSafeMethod = (method) => /^(get|head|options|trace)$/.test(method.toLowerCase());
