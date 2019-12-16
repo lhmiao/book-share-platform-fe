7c6fe98207741fd1681bed49845d46e97107a8eb
lhmiao <2696254238@qq.com>
2019-12-17 02:58:45 +0800

    feat: 添加 user 接口

--- src/apis/user.js
+++ src/apis/user.js
@@ -2,17 +2,44 @@ import Client from './Client';
 
 const client = new Client();
 
+// 检查登录
+export function checkLogin() {
+  const url = '/user/check_login';
+  return client.get(url, undefined, { useErrorTip: false });
+}
+
+// 获取安全问题答案
+export function fetchSecurityQuestion(params) {
+  const url = '/user/security_question';
+  return client.get(url, params);
+}
+
+// 登录
 export function login(params) {
   const url = '/user/login';
   return client.post(url, params);
 }
 
+// 注册
+export function register(params) {
+  const url = '/user/register';
+  return client.post(url, params);
+}
+
+// 登出
 export function logout() {
   const url = '/user/logout';
   return client.get(url);
-} 
+}
 
-export function getSessionInfo() {
-  const url = '/user/session';
-  return client.get(url);
+// 修改密码
+export function modifyPassword(params) {
+  const url = '/user/password';
+  return client.put(url, params);
+}
+
+// 修改用户信息
+export function modifyUserInfo(params) {
+  const url = '/user';
+  return client.patch(url, params);
 }
