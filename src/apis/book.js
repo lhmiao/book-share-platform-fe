47cfd9e6351001c94236dedf0cea9e8702d865af
lhmiao <2696254238@qq.com>
2019-12-21 04:54:21 +0800

    feat: 添加创建图书接口

--- src/apis/book.js
+++ src/apis/book.js
@@ -7,3 +7,9 @@ export function fetchBookList(params) {
   const url = '/book';
   return client.get(url, params);
 }
+
+// 创建图书
+export function createBook(params) {
+  const url = '/book';
+  return client.post(url, params, { useFormData: true });
+}
