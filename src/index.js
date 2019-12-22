33b9beb3b83162defcf897c56908e5af123c183d
lhmiao <2696254238@qq.com>
2019-12-23 04:26:18 +0800

    feat: moment 设置为中文

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+
+moment.locale('zh-cn');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
